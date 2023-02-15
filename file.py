aadab4f136357da4f5dfddf2a9c8838e6ca45857
AtesiT <atesit@mail.ru>
2023-02-15 22:28:44 +0300

    add if and base false arguments (and i wanna sleep)

--- file.py
+++ file.py
@@ -6,10 +6,29 @@ def generate_password(min_length, numbers=True, special_characters=True):
     digits = string.digits
     special = string.punctuation
 
-    print(letters, digits, special)
-
 #           По умолчанию пароль генерируется с числами, символами
 #           Если (<любое число>, False, False) - пароль будет только с буквами
 
 
-generate_password(10, False, False)
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+
+#           Добавятся числа и символы, если они нужны по условию
+
+
+    pwd = ''
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    
+    
+
+    print(letters, digits, special)
+
+
+
+generate_password(10)
