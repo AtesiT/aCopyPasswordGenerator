e5e04778da807b3c85e12c3cf513fe9de6c1d7b7
AtesiT <atesit@mail.ru>
2023-02-16 23:39:43 +0300

    did it

--- file.py
+++ file.py
@@ -24,11 +24,28 @@ def generate_password(min_length, numbers=True, special_characters=True):
     has_number = False
     has_special = False
 
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+        
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+
+
+    return pwd
     
-    
-
-    print(letters, digits, special)
 
 
-
-generate_password(10)
+min_length = int(input('Введите минимальную длину пароля: '))
+has_number = input('Вы желаете числа в пароле? Да/Нет ').lower() == 'да'
+has_special = input('Вы желаете числа в пароле? Да/Нет ').lower() == 'да'
+pwd = generate_password(min_length, has_number, has_special)
+print('Ваш пароль:', pwd)
